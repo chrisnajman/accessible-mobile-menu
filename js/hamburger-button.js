97637b97a4ebe3521d8e5a45b2508e2debf5d841
Chris Najman <christopher.najman@gmail.com>
2024-01-08 22:29:13 +0000

    Add info text to menu button (visually hidden). Clean up redundant css vars

--- js/hamburger-button.js
+++ js/hamburger-button.js
@@ -1,13 +1,18 @@
 export default function btnHamburger() {
   const hamburgerButton = document.getElementById("hamburger-button")
+  const btnTxt = document.getElementById("btn-txt")
   const primaryNavigation = document.getElementById("primary-navigation")
   const siteNavigation = document.getElementById("site-navigation")
 
-  hamburgerButton.addEventListener("click", () => {
+  hamburgerButton.addEventListener("click", (e) => {
     const isExpanded = hamburgerButton.getAttribute("aria-expanded")
-    isExpanded === "false"
-      ? hamburgerButton.setAttribute("aria-expanded", "true")
-      : hamburgerButton.setAttribute("aria-expanded", "false")
+    if (isExpanded === "false") {
+      hamburgerButton.setAttribute("aria-expanded", "true")
+      btnTxt.textContent = "Close menu"
+    } else {
+      hamburgerButton.setAttribute("aria-expanded", "false")
+      btnTxt.textContent = "Expand menu"
+    }
     siteNavigation.classList.toggle("menu-hidden")
     siteNavigation.classList.toggle("menu-visible")
     primaryNavigation.classList.toggle("padding")
